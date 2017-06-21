78b7834a9b5fed401a355bc1cf19ac34552dae33
Kim Phillips <kim.phillips@arm.com>
2017-06-21 10:59:14 -0500

    clean up auxtrace.c from pr_ debugging

--- tools/perf/arch/arm/util/auxtrace.c
+++ tools/perf/arch/arm/util/auxtrace.c
@@ -31,16 +31,11 @@ struct auxtrace_record
 	struct perf_evsel *evsel;
 	bool found_etm = false, found_spe = false;
 
-	pr_err("%s %d: \n", __func__, __LINE__);
-	
 	cs_etm_pmu = perf_pmu__find(CORESIGHT_ETM_PMU_NAME);
 	arm_spe_pmu = perf_pmu__find(ARM_SPE_PMU_NAME);
 
-	pr_err("%s %d: cs_etm_pmu %p\n", __func__, __LINE__, cs_etm_pmu);
-	pr_err("%s %d: arm_spe_pmu %p\n", __func__, __LINE__, arm_spe_pmu);
 	if (evlist) {
 		evlist__for_each_entry(evlist, evsel) {
-			pr_err("%s %d: \n", __func__, __LINE__);
 			if (cs_etm_pmu &&
 			    evsel->attr.type == cs_etm_pmu->type)
 				found_etm = true;
@@ -50,25 +45,20 @@ struct auxtrace_record
 		}
 	}
 
-
-	pr_err("%s %d: \n", __func__, __LINE__);
 	if (found_etm && found_spe) {
-		pr_err("ARM Coresight ETM and SPE may not be used together\n");
-		*err = -EINVAL;
+		pr_err("Concurrent ARM Coresight ETM and SPE operation not currently supported\n");
+		*err = -EOPNOTSUPP;
 		return NULL;
 	}
 
-	pr_err("%s %d: \n", __func__, __LINE__);
 	if (found_etm)
 		return cs_etm_record_init(err);
 
-	pr_err("%s %d: \n", __func__, __LINE__);
 	if (found_spe)
 		return arm_spe_recording_init(err);
 
-	pr_err("%s %d: \n", __func__, __LINE__);
 	/*
-	 * Clear 'err' even if we haven't found a cs_etm event - that way perf
+	 * Clear 'err' even if we haven't found an event - that way perf
 	 * record can still be used even if tracers aren't present.  The NULL
 	 * return value will take care of telling the infrastructure HW tracing
 	 * isn't available.
