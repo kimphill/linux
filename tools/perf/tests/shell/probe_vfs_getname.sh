53bb2f170921e0cc09775730981bb87f1035410d
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-08-04 14:18:29 -0300

    perf test shell: Move vfs_getname probe function to lib
    Multiple tests will be able to reuse these functions, to test things
    like perf report, 'trace', etc, using this probe.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-96cq8buz0rpxwqy2mbbudq7w@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/shell/probe_vfs_getname.sh
+++ tools/perf/tests/shell/probe_vfs_getname.sh
@@ -2,27 +2,7 @@
 #
 # Arnaldo Carvalho de Melo <acme@kernel.org>, 2017
 
-perf probe -l | grep -q probe:vfs_getname
-had_vfs_getname=$?
-
-cleanup_probe_vfs_getname() {
-	if [ $had_vfs_getname -eq 1 ] ; then
-		perf probe -q -d probe:vfs_getname
-	fi
-}
-
-add_probe_vfs_getname() {
-	local verbose=$1
-	if [ $had_vfs_getname -eq 1 ] ; then
-		perf probe $verbose 'vfs_getname=getname_flags:72 pathname=result->name:string' || return 1
-	fi
-	return 0
-}
-
-skip_if_no_debuginfo() {
-	add_probe_vfs_getname -v 2>&1 | grep -q "^Failed to find the path for kernel" && return 2
-	return 1
-}
+. $(dirname $0)/lib/probe_vfs_getname.sh
 
 add_probe_vfs_getname || skip_if_no_debuginfo
 err=$?
