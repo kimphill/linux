4a78cf8fd51479c5b6c19a1a2548b44897353c5d
Kim Phillips <kim.phillips@arm.com>
2017-10-20 18:01:43 -0500

    more ccn: evname additions

--- tools/perf/arch/arm64/util/evsel.c
+++ tools/perf/arch/arm64/util/evsel.c
@@ -11,25 +11,25 @@ static int ccn_strerror(struct perf_evsel *evsel,
 			struct target *target __maybe_unused,
 			int err, char *msg, size_t size)
 {
+	const char *evname = perf_evsel__name(evsel);
 	struct perf_event_attr *attr = &evsel->attr;
 
 	switch (err) {
 	case EOPNOTSUPP:
 		if (attr->sample_period)
-			return scnprintf(msg, size, "Sampling not supported, try 'perf stat'\n");
+			return scnprintf(msg, size, "%s: Sampling not supported, try 'perf stat'\n", evname);
 		if (target->per_thread)
-			return scnprintf(msg, size, "Can't provide per-task data!\n");
-		return scnprintf(msg, size, "*FILL ME IN*\n");
+			return scnprintf(msg, size, "%s: Can't provide per-task data!\n", evname);
 		break;
 	case EINVAL:
-		if (//(attr->sample_type & PERF_SAMPLE_BRANCH_STACK) ||
+		if ((attr->sample_type & PERF_SAMPLE_BRANCH_STACK) ||
 			attr->exclude_user ||
 			attr->exclude_kernel || attr->exclude_hv ||
 			attr->exclude_idle || attr->exclude_host ||
 			attr->exclude_guest)
-			return scnprintf(msg, size, "Can't exclude execution levels!\n");
+			return scnprintf(msg, size, "%s: Can't exclude execution levels!\n", evname);
 		return scnprintf(msg, size,
-	"Invalid MN / XP / node ID, or node type, or node/XP port / vc or event, or mixed PMU group. See dmesg for details\n");
+	"%s: Invalid MN / XP / node ID, or node type, or node/XP port / vc or event, or mixed PMU group. See dmesg for details\n", evname);
 		break;
 	default:
 		break;
@@ -104,7 +104,7 @@ int perf_evsel__suppl_strerror(struct perf_evsel *evsel,
 	const char *evname = perf_evsel__name(evsel);
 
 	pr_err("%s %d: \n", __func__, __LINE__);
-	if (strstarts(evname, "ccn/"))
+	if (strstarts(evname, "ccn"))
 		return ccn_strerror(evsel, target, err, msg, size);
 
 #ifdef HAVE_AUXTRACE_SUPPORT
