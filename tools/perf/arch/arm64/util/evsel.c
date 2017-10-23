7f1a4b1f3c7ea76492a4a6aabfcc780213d1904d
Kim Phillips <kim.phillips@arm.com>
2017-10-23 09:13:55 -0500

    ccn: tagging messages we dont see (or we do on dmesg): arm-ccn e8000000.ccn: Invalid type 0x16 for node 3!
    [174539.436603] arm-ccn e8000000.ccn: Sampling not supported!
    [174582.290889] arm-ccn e8000000.ccn: Sampling not supported!
    [174586.875655] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174586.882054] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174586.888279] arm-ccn e8000000.ccn: Invalid type 0x16 for node 4!
    [174586.894291] arm-ccn e8000000.ccn: Invalid type 0x16 for node 4!
    [174632.767871] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174632.774070] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174632.780302] arm-ccn e8000000.ccn: Invalid type 0x16 for node 3!
    [174632.786315] arm-ccn e8000000.ccn: Invalid type 0x16 for node 3!
    [174641.572311] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174641.578529] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174884.466341] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174884.472538] arm-ccn e8000000.ccn: Can't exclude execution levels!
    [174884.484786] arm-ccn e8000000.ccn: Invalid type 0x16 for node 3!

--- tools/perf/arch/arm64/util/evsel.c
+++ tools/perf/arch/arm64/util/evsel.c
@@ -18,16 +18,33 @@ static int ccn_strerror(struct perf_evsel *evsel,
 	case EOPNOTSUPP:
 		if (attr->sample_period)
 			return scnprintf(msg, size, "%s: Sampling not supported, try 'perf stat'\n", evname);
-		if (target->per_thread)
+		if (target->cpu < 0)
 			return scnprintf(msg, size, "%s: Can't provide per-task data!\n", evname);
 		break;
 	case EINVAL:
+		/* we never see the below because the fallback gets called on the exclude_guest being set, and if that didn't work, we just get:
+
+
+perf/perf stat -e ccn/rni_rdata_beats_p0,node=3/ sleep 1
+
+ Performance counter stats for 'system wide':
+
+   <not supported>      ccn/rni_rdata_beats_p0,node=3/                                   
+
+       1.003762498 seconds time elapsed
+
+show how doc example fails:
+
+invalid or unsupported event: 'ccn/cycles/,ccn/xp_valid_flit,xp=1,port=0,vc=1,dir=1/'
+
+*/
 		if ((attr->sample_type & PERF_SAMPLE_BRANCH_STACK) ||
 			attr->exclude_user ||
 			attr->exclude_kernel || attr->exclude_hv ||
 			attr->exclude_idle || attr->exclude_host ||
 			attr->exclude_guest)
 			return scnprintf(msg, size, "%s: Can't exclude execution levels!\n", evname);
+		/* we never see the below */
 		return scnprintf(msg, size,
 	"%s: Invalid MN / XP / node ID, or node type, or node/XP port / vc or event, or mixed PMU group. See dmesg for details\n", evname);
 		break;
