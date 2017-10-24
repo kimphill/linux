d18d93d1712f947db35c68a33a4eb854c8eb6918
Kim Phillips <kim.phillips@arm.com>
2017-10-24 03:04:08 -0500

    perf tool: stat: say more about why event is not supported by the kernel
    Call perf_evsel__open_strerror() to potentially expand upon why the
    event was "not supported by the kernel."  See the enhanced '-v' example
    output messages in the next patch.
    
    > Nit: it would be better if the commit message was self-contained. It might
    > also be worth moving some of the commit meesage for the final patch out into
    > a cover letter, since there's an awful lot in there.
    
    > Hmm, perf_evsel__open_strerror can already get called a few lines later, so
    > I don't think this change is right. It looks like the intention here is that
    > we can push ahead skipping unsupported events unless they are the leader of
    > a populated group. If this isn't working as intended, then it looks like we
    > need a helper to identify unsupported events instead.
    
    removed ui_warning with generic "not supported by the kernel," used ui_warning
    instead for new call.
    
    Not sure if this is about fixing skipping unsupported events, more detailing
    error messaging for those events....
    
    Signed-off-by: Kim Phillips <kim.phillips@arm.com>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -625,9 +625,11 @@ try_again:
 			if (errno == EINVAL || errno == ENOSYS ||
 			    errno == ENOENT || errno == EOPNOTSUPP ||
 			    errno == ENXIO) {
-				if (verbose > 0)
-					ui__warning("%s event is not supported by the kernel.\n",
-						    perf_evsel__name(counter));
+				if (verbose > 0) {
+					perf_evsel__open_strerror(counter, &target,
+								  errno, msg, sizeof(msg));
+					ui__warning("%s\n", msg);
+				}
 				counter->supported = false;
 
 				if ((counter->leader != counter) ||
