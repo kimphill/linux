8582342f362682f82c54b30ba19e15083fa8adc4
Kim Phillips <kim.phillips@arm.com>
2018-02-27 00:13:20 -0600

    fix native build problem
      CC       arch/arm/tests/arch-tests.o
    In file included from arch/arm/tests/arch-tests.c:4:0:
    /home/kim/linux/tools/perf/arch/arm/include/arch-tests.h:8:5: error: redundant redeclaration of ‘test__arch_unwind_sample’ [-Werror=redundant-decls]
     int test__arch_unwind_sample(struct perf_sample *sample,
         ^
    In file included from arch/arm/tests/arch-tests.c:3:0:
    /home/kim/linux/tools/perf/tests/tests.h:112:5: note: previous declaration of ‘test__arch_unwind_sample’ was here
     int test__arch_unwind_sample(struct perf_sample *sample,
         ^
    cc1: all warnings being treated as errors

--- tools/perf/arch/arm/include/arch-tests.h
+++ tools/perf/arch/arm/include/arch-tests.h
@@ -5,8 +5,6 @@
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
 struct thread;
 struct perf_sample;
-int test__arch_unwind_sample(struct perf_sample *sample,
-			     struct thread *thread);
 #endif
 
 extern struct test arch_tests[];
