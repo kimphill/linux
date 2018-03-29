7c259c42932af7626adfa5507c2e891403133e15
Kim Phillips <kim.phillips@arm.com>
2018-03-29 14:20:03 -0500

    add make scripts
    make scripts: serious fun enhancements
    
    turn off RANDOMIZE_BASE until stack trace gets fixed
    
    get image size back down to somethign bootable
    
    make juno script goodness
    
    test child finished with -1
    ---- end ----
    vmlinux symtab matches kallsyms: FAILED!
    
    [   59.570022] BUG: sleeping function called from invalid context at ../mm/slab.h:421
    [   59.577536] in_atomic(): 1, irqs_disabled(): 128, pid: 0, name: swapper/4
    [   59.584261] no locks held by swapper/4/0.
    [   59.588237] CPU: 4 PID: 0 Comm: swapper/4 Not tainted 4.17.0-rc2-00015-g68281641035b-dirty #61
    [   59.596769] Hardware name: ARM LTD ARM Juno Development Platform/ARM Juno Development Platform, BIOS EDK II Jan 23 2017
    [   59.607449] Call trace:
    [   59.609882]  dump_backtrace+0x0/0x1d8
    [   59.613512]  show_stack+0x24/0x30
    [   59.616799]  dump_stack+0x9c/0xbc
    [   59.620087]  ___might_sleep+0x110/0x130
    [   59.623889]  __might_sleep+0x58/0x90
    [   59.627435]  __kmalloc+0x2b0/0x480
    [   59.630808]  crng_reseed+0x18c/0x378
    [   59.634351]  credit_entropy_bits+0x374/0x380
    [   59.638583]  add_interrupt_randomness+0x190/0x208
    [   59.643246]  handle_irq_event_percpu+0x50/0x98
    [   59.647650]  handle_irq_event+0x50/0x80
    [   59.651453]  handle_fasteoi_irq+0xb0/0x188
    [   59.655513]  generic_handle_irq+0x34/0x50
    [   59.659486]  __handle_domain_irq+0x68/0xc0
    [   59.663546]  gic_handle_irq+0x6c/0xc8
    [   59.667176]  el1_irq+0xb0/0x128
    [   59.670292]  cpuidle_enter_state+0xbc/0x368
    [   59.674438]  cpuidle_enter+0x34/0x48
    [   59.677982]  call_cpuidle+0x44/0x70
    [   59.681440]  do_idle+0x230/0x290
    [   59.684640]  cpu_startup_entry+0x2c/0x30
    [   59.688529]  secondary_start_kernel+0x18c/0x1c8
    [   59.693104] random: crng init done
    
    more script fixes
    
    and for test 1
    
    tc2 and juno merge, make shared config. up dmesg size
    
    set CONFIG_DEBUG_KMEMLEAK_TEST
    
    add dynamic debug to config
    
    makescripts: enable STM for CORESIGHT_STM
    
    script: enable CORESIGHT =y, just for now.
    
    scripts: make tc2 enable coresight - just for now
    
    scripts: enable ETM, move coresight to shared config
    
    SCRIPTS: coresight-dynamic-replicator module
    
    SCRIPTS: ETM4X = m
    
    SCRIPTS: ETM3 =m BUUUUT ETM 4 = y
    
    SCRIPTS: ETM4 = m
    
    SCRIPTS: ETBV10 = m
    
    SCRIPTS: SINK_TPIU = m
    
    SCRIPTS: LINK_AND_SINK_TMC = m
    
    SCRIPTS: LINKS_AND_SINKS = m
    
    SCRIPTS: CORESIGHT (CORE!) = m

---
+++ shared-config.sh
@@ -0,0 +1,28 @@
+
+../scripts/config --set-val CONFIG_LOG_BUF_SHIFT 17
+../scripts/config --set-val CONFIG_LOG_CPU_MAX_BUF_SHIFT 15
+../scripts/config --set-val CONFIG_PRINTK_SAFE_LOG_BUF_SHIFT 16
+
+../scripts/config -e CONFIG_DEBUG_KMEMLEAK
+../scripts/config -e CONFIG_DEBUG_KMEMLEAK_TEST
+../scripts/config -e CONFIG_DMA_API_DEBUG
+
+../scripts/config -e CONFIG_DYNAMIC_DEBUG
+
+
+#coresight
+../scripts/config -e CONFIG_STM
+../scripts/config -m CONFIG_CORESIGHT
+#dunno, putting all in:
+../scripts/config -m CONFIG_CORESIGHT_LINK_AND_SINK_TMC
+../scripts/config -m CONFIG_CORESIGHT_SINK_TPIU
+../scripts/config -m CONFIG_CORESIGHT_SINK_ETBV10
+../scripts/config -m CONFIG_CORESIGHT_LINKS_AND_SINKS
+../scripts/config -m CONFIG_CORESIGHT_SOURCE_ETM3X
+../scripts/config -m CONFIG_CORESIGHT_SOURCE_ETM4X
+#../scripts/config -e CONFIG_CORESIGHT_QCOM_REPLICATOR
+../scripts/config -m CONFIG_CORESIGHT_DYNAMIC_REPLICATOR
+../scripts/config -m CONFIG_CORESIGHT_STM
+../scripts/config -m CONFIG_CORESIGHT_CPU_DEBUG
+
+
