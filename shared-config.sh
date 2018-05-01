465752b67a75da4dc88e59cf80e38e88e0159c80
Kim Phillips <kim.phillips@arm.com>
2018-05-01 11:06:54 -0500

    set CONFIG_DEBUG_KMEMLEAK_TEST

--- shared-config.sh
+++ shared-config.sh
@@ -4,4 +4,5 @@
 ../scripts/config --set-val CONFIG_PRINTK_SAFE_LOG_BUF_SHIFT 16
 
 ../scripts/config -e CONFIG_DEBUG_KMEMLEAK
+../scripts/config -e CONFIG_DEBUG_KMEMLEAK_TEST
 ../scripts/config -e CONFIG_DMA_API_DEBUG
