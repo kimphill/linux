a5ca27d62e2156ff903d721620d8f29ac50e5c71
Kim Phillips <kim.phillips@arm.com>
2018-05-21 11:12:16 -0500

    add BPF to shared config

--- shared-config.sh
+++ shared-config.sh
@@ -9,3 +9,31 @@
 
 ../scripts/config -e CONFIG_DYNAMIC_DEBUG
 
+
+../scripts/config -e CONFIG_KPROBES
+../scripts/config -e CONFIG_KPROBE_EVENTS
+
+../scripts/config -e CONFIG_BPF_SYSCALL
+../scripts/config -m CONFIG_TEST_BPF
+../scripts/config -e CONFIG_BPF_EVENTS
+
+
+
+#exit <<  can't do that here, it exist the caller, maybe try not sourcing this?
+
+#kim@juno perf perf/core$ sudo ./perf test -v bpf
+
+#bpf: config program 'func=do_epoll_wait'
+#symbol:do_epoll_wait file:(null) line:0 offset:0 return:0 lazy:(null)
+#bpf: config 'func=do_epoll_wait' is ok
+#No kprobe blacklist support, ignored
+#Looking at the vmlinux_path (8 entries long)
+#Using /boot/vmlinux for symbols
+#Open Debuginfo file: /boot/vmlinux
+#Try to find probe point from debuginfo.
+#Matched function: do_epoll_wait [1e8f460]
+#Probe point found: do_epoll_wait+0
+#Found 1 probe_trace_events.
+#Opening /sys/kernel/debug/tracing/kprobe_events write=1
+#kprobe_events file does not exist - please rebuild kernel with CONFIG_KPROBE_EVENTS.
+
